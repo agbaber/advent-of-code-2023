a7197efdfdc672e061a219104dbe4a8f6f63f9d9
Andrew Baber <baber@andrewbaber.com>
2023-12-01 12:23:35 -0500

    revert to part 1 for refactor

--- day_01/run1.rb
+++ day_01/run1.rb
@@ -6,8 +6,7 @@ class Runner
 
   def run
     @input.each_line do |line|
-      digits = sub_words(line).select { |c| c =~ /\d/ }
-      # digits = line.split('').select { |c| c =~ /\d/ }
+      digits = line.split('').select { |c| c =~ /\d/ }
       value = "#{digits.first}#{digits.last}".to_i
       puts value
       @output << value
@@ -17,54 +16,6 @@ class Runner
   end
 
   private
-
-  def sub_words(input)
-    output = []
-    split = input.split('')
-    split.each_with_index do |l,i|
-      case l
-      when 'o'
-        if split[i..i+2].join == 'one'
-          output << '1'
-        end
-      when 't'
-        if split[i..i+2].join == 'two'
-          output << '2'
-        end
-
-        if split[i..i+4].join == 'three'
-          output << '3'
-        end
-      when 'f'
-        if split[i..i+3].join == 'four'
-          output << '4'
-        end
-        if split[i..i+3].join == 'five'
-          output << '5'
-        end
-      when 's'
-        if split[i..i+2].join == 'six'
-          output << '6'
-        end
-        if split[i..i+4].join == 'seven'
-          output << '7'
-        end
-      when 'e'
-        if split[i..i+4].join == 'eight'
-          output << '8'
-        end
-      when 'n'
-        if split[i..i+3].join == 'nine'
-          output << '9'
-        end
-      else
-        if l =~ /\d/
-          output << l
-        end
-      end
-    end
-    
-    output
 
   end
 end
