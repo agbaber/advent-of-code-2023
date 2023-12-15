39ca2a36535489325bdea133c703cd39be71fb4a
Andrew Baber <baber@andrewbaber.com>
2023-12-15 09:17:12 -0500

    Day 15 part 1

--- day_15/run.rb
+++ day_15/run.rb
@@ -1,13 +1,36 @@
 class Runner
   def initialize(filename)
     @input = File.read(filename)
+    @hash_values = []
   end
 
   def run
+    @steps = @input.chomp.split(',')
+
+    @steps.each do |step|
+      @hash_values << get_hash_value(step)
+    end
+
+    @hash_values.inject(&:+)
   end
 
   private
 
+  def get_hash_value(step)
+    current_value = 0
+
+    step.codepoints do |c|
+      current_value += c
+      current_value = current_value * 17
+      current_value = current_value % 256
+    end
+
+    puts "#{step} becomes #{current_value}"
+
+    current_value
+  end
 end
 
-Runner.new('input.txt').run
+if Runner.new('test.txt').run == 1320
+  puts Runner.new('input.txt').run
+end
